9d681dbbaf56f8a032bb0f03d46844dda134da33
Rohit685 <mittalr685@gmail.com>
2024-03-03 17:14:34 -0600

    TOC looks better now

--- components/TOC.js
+++ components/TOC.js
@@ -14,13 +14,10 @@ const TOC = () => {
   			<div className={`fixed top-0 -left-2 z-50 ${isSidebarOpen ? 'bg-background rounded-2xl max-2xl:border' : 'bg-none'}`}>
                 <button class="relative bg-blue-500 mx-6 mt-2 fixed left-0 py-2 px-4 hover:bg-blue-700 text-white font-bold rounded-2xl" onClick={toggleSidebar}>{isSidebarOpen ? "Close" : "Open"}</button>
   			<div className={`px-6 pb-36 my-1 w-240 overflow-y-scroll h-screen text-lg list-disc ${isSidebarOpen ? 'block' : 'hidden'}`}>
-				<a href="/" className="text-2xl dark:text-gray-400">Home</a>
-					<h2 className="text-2xl dark:text-gray-400 mb-1">Getting Started</h2>
 					<div className="flex flex-col space-y-1">
-						<li><a href="/setupenv" className={"text-wrap"}>Setting up Development Environment</a></li>
-					</div>
-					<a href="/basics/" className="text-2xl dark:text-gray-400 mt-2 mb-1">Basics Of C#</a>
-					<div className="flex flex-col space-y-1">
+						<a href="/" className="text-2xl dark:text-gray-400">Home</a>
+						<a href="/setupenv" className="text-2xl dark:text-gray-400">Getting Started</a>
+						<a href="/basics/" className="text-2xl dark:text-gray-400 mt-2">Basics Of C#</a>
 						<li><a href="/basics/dataandvars">Variables & Data Types</a></li>
 						<li><a href="/basics/methods">Methods</a></li>
 						<li><a href="/basics/console">Console Functions</a></li>
