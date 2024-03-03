b411fc7f956f499c73ff0bf01bb98fe87b797373
Rohit685 <mittalr685@gmail.com>
2024-03-03 14:04:24 -0600

    sidebar fix

--- components/TOC.js
+++ components/TOC.js
@@ -11,9 +11,10 @@ const TOC = () => {
    
    return ( 
        <>
-		   <button class="bg-blue-500 ml-6 fixed left-0 py-2 px-4 hover:bg-blue-700 text-white font-bold rounded" onClick={toggleSidebar}>{isSidebarOpen ? "Close" : "Open"} Sidebar</button>
-  			<div className={`fixed left-0 p-6 bg-background rounded-2xl max-2xl:border mt-10 w-240 w-72 overflow-y-scroll h-full text-lg text-wrap list-disc ${isSidebarOpen ? 'block' : 'hidden'}`}>
-					<a href="/" className="text-2xl dark:text-gray-400 mb-1">Home</a>
+  			<div className={`fixed left-0 z-50 ${isSidebarOpen ? 'bg-background rounded-2xl max-2xl:border' : 'bg-none'}`}>
+                <button class="relative bg-blue-500 mx-6 mt-2 fixed left-0 py-2 px-4 hover:bg-blue-700 text-white font-bold rounded-2xl" onClick={toggleSidebar}>{isSidebarOpen ? "Close" : "Open"}</button>
+  			<div className={`px-6 pb-32 my-1 w-240 overflow-y-scroll h-screen text-lg list-disc ${isSidebarOpen ? 'block' : 'hidden'}`}>
+				<a href="/" className="text-2xl dark:text-gray-400">Home</a>
 					<h2 className="text-2xl dark:text-gray-400 mb-1">Getting Started</h2>
 					<div className="flex flex-col space-y-1">
 						<li><a href="/setupenv" className={"text-wrap"}>Setting up Development Environment</a></li>
@@ -42,6 +43,7 @@ const TOC = () => {
 						<li><a href={"/ds/lists"}>Lists</a></li>
 						<li><a href={"/ds/mp2p2"}>Mini Project 2 Part 2</a></li>
 					</div>
+			</div>
 			</div>
 	   </>
    )
