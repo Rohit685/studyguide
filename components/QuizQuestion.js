dae86165dee29a417a01e25512157c0d7f1c7bf0
Rohit685 <mittalr685@gmail.com>
2024-03-05 14:55:18 -0600

    fix: added null check for qCode

--- components/QuizQuestion.js
+++ components/QuizQuestion.js
@@ -13,7 +13,7 @@ const QuizQuestion = ({ question, qCode, aCode, explanation }) => {
       <div>
         <div className={`card outline-0 py-0 px-0 gap-2 my-0`}>
             <p className={`my-1`}>Putting it all together: {question}</p>
-            <SyntaxHighlighter language="csharp" code={qCode} style={nightOwl} useInlineStyles={true}/>
+            {qCode != null && <SyntaxHighlighter language="csharp" code={qCode} style={nightOwl} useInlineStyles={true}/> }
             <button className={"bg-blue-500 hover:bg-blue-700 text-white p-3 h-fit rounded-2xl max-xs:my-1"} onClick={toggleAnswer}>Toggle Answer</button>
         </div>
         {show && 
