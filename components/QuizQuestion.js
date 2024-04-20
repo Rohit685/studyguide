fc8e24b4dbb95e5e16d5e33ab44912edf0da3ac0
Rohit685 <mittalr685@gmail.com>
2024-04-19 23:46:48 -0500

    fix: inconsistent margins

--- components/QuizQuestion.js
+++ components/QuizQuestion.js
@@ -10,20 +10,34 @@ const QuizQuestion = ({ question, qCode, aCode, explanation }) => {
   };
 
   return (
-      <div>
-        <div className={`card outline-0 py-0 px-0`}>
-            <p>Putting it all together: {question}</p>
-            {qCode != null && <SyntaxHighlighter language="csharp" code={qCode} style={nightOwl} useInlineStyles={true}/> }
-            <button className={"bg-blue-600 hover:bg-blue-700 text-white p-3 h-fit rounded-2xl w-fit"} onClick={toggleAnswer}>{show ? "Close" : "Open"} Answer</button>
-        </div>
-        {show && 
-            <div className={`card rounded-xl`}>
-                <p>
-                    {explanation}
-                </p>
-                { aCode != null && <SyntaxHighlighter language="csharp" code={aCode} style={nightOwl} useInlineStyles={true}/> }
-            </div>
-        }
+      // <div className={"collapse collapse-arrow"}>
+      //     <input type={"checkbox"}/>
+      //   <div className={`card outline-0 py-0 px-0`}>
+      //       <p>Putting it all together: {question}</p>
+      //       {qCode != null && <SyntaxHighlighter language="csharp" code={qCode} style={nightOwl} useInlineStyles={true}/> }
+      //       <button className={"bg-blue-600 hover:bg-blue-700 text-white p-3 h-fit rounded-2xl w-fit"} onClick={toggleAnswer}>{show ? "Close" : "Open"} Answer</button>
+      //   </div>
+      //   {show && 
+      //       <div className={`card rounded-xl`}>
+      //           <p>
+      //               {explanation}
+      //           </p>
+      //           { aCode != null && <SyntaxHighlighter language="csharp" code={aCode} style={nightOwl} useInlineStyles={true}/> }
+      //       </div>
+      //   }
+      // </div>
+      <div className="collapse collapse-plus quizContainerColor my-3">
+          <input type="checkbox"/>
+          <div className="collapse-title">
+              <p>Putting it all together: {question}</p>
+              {qCode != null && <SyntaxHighlighter language="csharp" code={qCode} style={nightOwl} useInlineStyles={true}/> }
+          </div>
+          <div className="collapse-content">
+              <p>
+                  {explanation}
+              </p>
+              { aCode != null && <SyntaxHighlighter language="csharp" code={aCode} style={nightOwl} useInlineStyles={true}/> }
+          </div>
       </div>
   );
 };
