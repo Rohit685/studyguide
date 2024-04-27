2b5ccf2019cba6e18b4fc7e0a29f64a8b6600068
Rohit685 <mittalr685@gmail.com>
2024-04-27 14:17:49 -0500

    fix: incomplete question for arrays

--- pages/ds/arrays.js
+++ pages/ds/arrays.js
@@ -92,7 +92,7 @@ export default function Home() {
                                 As you can maybe see, with for each loops, you do <b>not</b> get access to the index the element has. Therefore, only use a for each 
                                 loop when you know that the index is <b>not</b> necessary.
                             </p>
-                            <QuizQuestion question={"Fill in the blank: "} qCode={quizQuestionQ} aCode={quizQuestionAnswer} 
+                            <QuizQuestion question={"Fill in the blank so that this code block only prints out numbers at an even index."} qCode={quizQuestionQ} aCode={quizQuestionAnswer} 
                                           explanation={"Using the index 'i' and modulus operator, we can decide whether to that index is even by checking whether the remainder is 0" +
                                               "After determining whether to use the index, we use string concatenation to get the element at that index."} genericID={4}
                                         correctAnswer={"i % 2 == 0"}/>
